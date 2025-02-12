6cc8afeb28fecb932b3e55a71ae8430e38496dd4
Rishiraj Bhattacharjee <72534709+RISHIRAJ-BHATTACHARJEE@users.noreply.github.com>
2025-02-12 20:16:36 +0530

    Update sign-in-card.tsx

--- src/features/auth/components/sign-in-card.tsx
+++ src/features/auth/components/sign-in-card.tsx
@@ -91,33 +91,6 @@ const SignInCard = () => {
         <DottedSeparator />
       </div>
 
-      <CardContent className="p-7 flex flex-col gap-y-4">
-        <Button
-          disabled={isPending}
-          variant="secondary"
-          size="lg"
-          className="w-full"
-          onClick={() => signUpWithGoogle()}
-        >
-          <FcGoogle className="mr-2 size-5" />
-          Login with Google
-        </Button>
-        <Button
-          disabled={isPending}
-          onClick={() => signUpWithGithub()}
-          variant="secondary"
-          size="lg"
-          className="w-full"
-        >
-          <FaGithub className="mr-2 size-5" />
-          Login with Github
-        </Button>
-      </CardContent>
-
-      <div className="px-7">
-        <DottedSeparator />
-      </div>
-
       <CardContent className="p-7 flex items-center justify-center">
         <p>
           Don&apos;t have an account?{" "}
